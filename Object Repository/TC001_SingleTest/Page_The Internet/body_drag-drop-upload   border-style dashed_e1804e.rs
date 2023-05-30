88f58e0c47e2e72ff2517dca3b3862cec3a87e13
Indah Mutiah MZ <indah@cnt.id>
2023-05-30 18:03:56 +0700

    Single test Case

---
+++ Object Repository/TC001_SingleTest/Page_The Internet/body_drag-drop-upload   border-style dashed_e1804e.rs
@@ -0,0 +1,254 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>body_drag-drop-upload   border-style dashed_e1804e</name>
+   <tag></tag>
+   <elementGuidId>11643eb2-c7c7-4078-a0c7-c88f3a2d7498</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>body</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//body</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>body</value>
+      <webElementGuid>d9ddea45-cc8a-4128-832f-be6370de9b86</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+    
+      
+      
+        
+      
+        
+      
+        
+      
+      
+    
+    
+      
+      
+        
+
+#drag-drop-upload {
+  border-style: dashed;
+  border-width: 5px;
+  border-color: red;
+  height: 400px;
+  width: 400px;
+}
+
+
+$('.dz-success-mark').remove();
+
+
+File Uploader
+Choose a file on your system and then click upload. Or, drag and drop a file into the area below.
+  
+    
+    
+    
+  
+
+
+
+
+
+  
+    
+      
+    
+    ✔
+  
+
+
+  $('div#drag-drop-upload').dropzone({
+    url: '/upload',
+    previewTemplate: $('#preview-template').html()
+  });
+
+
+      
+    
+    
+      
+        
+        Powered by Elemental Selenium
+      
+    
+  
+
+id(&quot;file-upload&quot;)</value>
+      <webElementGuid>719aab3f-58dc-4522-b04d-0f43c18d0968</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[@class=&quot;no-js&quot;]/body[1]</value>
+      <webElementGuid>d248797c-e1c0-4853-9913-0fbd9e385cbe</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//body</value>
+      <webElementGuid>47598f67-7196-47a0-8376-c53f556abcf6</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//body[(text() = concat(&quot;
+    
+      
+      
+        
+      
+        
+      
+        
+      
+      
+    
+    
+      
+      
+        
+
+#drag-drop-upload {
+  border-style: dashed;
+  border-width: 5px;
+  border-color: red;
+  height: 400px;
+  width: 400px;
+}
+
+
+$(&quot; , &quot;'&quot; , &quot;.dz-success-mark&quot; , &quot;'&quot; , &quot;).remove();
+
+
+File Uploader
+Choose a file on your system and then click upload. Or, drag and drop a file into the area below.
+  
+    
+    
+    
+  
+
+
+
+
+
+  
+    
+      
+    
+    ✔
+  
+
+
+  $(&quot; , &quot;'&quot; , &quot;div#drag-drop-upload&quot; , &quot;'&quot; , &quot;).dropzone({
+    url: &quot; , &quot;'&quot; , &quot;/upload&quot; , &quot;'&quot; , &quot;,
+    previewTemplate: $(&quot; , &quot;'&quot; , &quot;#preview-template&quot; , &quot;'&quot; , &quot;).html()
+  });
+
+
+      
+    
+    
+      
+        
+        Powered by Elemental Selenium
+      
+    
+  
+
+id(&quot;file-upload&quot;)&quot;) or . = concat(&quot;
+    
+      
+      
+        
+      
+        
+      
+        
+      
+      
+    
+    
+      
+      
+        
+
+#drag-drop-upload {
+  border-style: dashed;
+  border-width: 5px;
+  border-color: red;
+  height: 400px;
+  width: 400px;
+}
+
+
+$(&quot; , &quot;'&quot; , &quot;.dz-success-mark&quot; , &quot;'&quot; , &quot;).remove();
+
+
+File Uploader
+Choose a file on your system and then click upload. Or, drag and drop a file into the area below.
+  
+    
+    
+    
+  
+
+
+
+
+
+  
+    
+      
+    
+    ✔
+  
+
+
+  $(&quot; , &quot;'&quot; , &quot;div#drag-drop-upload&quot; , &quot;'&quot; , &quot;).dropzone({
+    url: &quot; , &quot;'&quot; , &quot;/upload&quot; , &quot;'&quot; , &quot;,
+    previewTemplate: $(&quot; , &quot;'&quot; , &quot;#preview-template&quot; , &quot;'&quot; , &quot;).html()
+  });
+
+
+      
+    
+    
+      
+        
+        Powered by Elemental Selenium
+      
+    
+  
+
+id(&quot;file-upload&quot;)&quot;))]</value>
+      <webElementGuid>3964b385-abea-4fcb-b119-a6748fda880e</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
