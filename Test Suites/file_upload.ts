5d7907fecd7670946cefc4f8cf450a6bd2258152
Indah Mutiah MZ <indah@cnt.id>
2023-05-30 20:11:27 +0700

    TC002 Driven Test

---
+++ Test Suites/file_upload.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>file_upload</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e70afcc4-e46b-42ce-a45c-6c690e079156</testSuiteGuid>
+   <testCaseLink>
+      <guid>bb9fa3f1-1c7e-407c-8e46-416eb5c313a4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC002_DrivenTestingCase</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>85be59e7-254e-4d4d-94c9-bd81fb8389db</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/file</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>85be59e7-254e-4d4d-94c9-bd81fb8389db</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>URLDIR</value>
+         <variableId>3e5e5a54-6238-4256-aee0-2c80afc28486</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
